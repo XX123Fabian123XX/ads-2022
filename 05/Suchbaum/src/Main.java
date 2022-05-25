f7109509fcf222d2b6df11af7911b2057261c808
Marcus <m.wiebe2000@gmail.com>
2022-05-26 00:34:00 +0200

    add task three but not operations from tree

--- 05/Suchbaum/src/Main.java
+++ 05/Suchbaum/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
         Tree<String> tree1 = new Tree<>(new Node<String>("tree1"));
@@ -11,6 +13,87 @@ public class Main {
 
         Tree<String> joinedTree = Tree.<String>bin(tree1, testNode, tree2);
 
+        //Task 3
+        Node<Integer> root = new Node<>(50);
+        Tree<Integer> leftTree = new Tree<>(new Node<Integer>(25));
+        Tree<Integer> rightTree = new Tree<>(new Node<Integer>(75));
 
+        Tree<Integer> searchTree = Tree.<Integer>bin(leftTree, root, rightTree);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please choose an action");
+        System.out.println("i: insert node");
+        System.out.println("d: delete node");
+        System.out.println("m: modify node");
+        System.out.println("P: preorder");
+        System.out.println("I: inorder");
+        searchTree.insert(13);
+        searchTree.insert(10);
+        searchTree.insert(37); // exclude for showing
+        searchTree.insert(63);
+        searchTree.insert(87);
+        searchTree.insert(95);
+        searchTree.insert(80);
+        searchTree.insert(83);
+        
+        while(true) {
+            System.out.println("\nPlease enter the action");
+            String input = scanner.nextLine();
+            if(input.equals("i")) {
+                while(true) {
+                    System.out.println("Please enter the value to be entered");
+                    String value = scanner.nextLine();
+                    int number;
+                    try {
+                        number = Integer.parseInt(value);
+                        searchTree.insert(number);
+                        break;
+                    } catch(NumberFormatException e) {}
+                }
+            }
+            else if(input.equals("d")) {
+                while(true) {
+                    System.out.println("Please enter the value to be entered");
+                    String value = scanner.nextLine();
+                    int number;
+                    try {
+                        number = Integer.parseInt(value);
+                        searchTree.delete(number);
+                        break;
+                    } catch(NumberFormatException e) {}
+                }
+            }
+            else if(input.equals("m")) {
+                while(true) {
+                    System.out.println("Please enter the key to be modified");
+                    String oldString = scanner.nextLine();
+                    int oldKey;
+                    try {
+                        oldKey = Integer.parseInt(oldString);
+                        while(true) {
+                            System.out.println("Please enter the new key");
+                            String newString = scanner.nextLine();
+                            int newKey;
+                            try {
+                                newKey = Integer.parseInt(newString);
+                                searchTree.modify(oldKey, newKey);
+                                break;
+                            } catch(NumberFormatException e) {}
+                        }
+                        break;
+                    } catch(NumberFormatException e) {}
+                }
+                
+            }
+            else if(input.equals("P")) {
+                searchTree.preOrder();
+            }
+            else if(input.equals("I")) {
+                searchTree.inOrder();
+            }
+            else if(input.equals(" ")) {
+                break;
+            }
+        }
+        scanner.close();
     }
 }
--- 05/Suchbaum/src/Tree.java
+++ 05/Suchbaum/src/Tree.java
@@ -43,6 +43,189 @@ public class Tree<T> {
             return this.rootNode;
         }
 
+        private void preOrder(Node<T> node) {
+            if(node == null) {
+                return;
+            }
+            printDetails(node);
+            preOrder(node.getLeftChild());
+            preOrder(node.getRightChild());
+        }
+
+        /**
+         * wrapper to start recursive function
+         */
+        public void preOrder() {
+            preOrder(this.rootNode);
+        }
+
+        private void printDetails(Node<T> node) {
+            System.out.print(node + "(" + getHeight(node) + "," + getBalance(node) + ") : ");
+            System.out.print(node.getLeftChild() != null ? node.getLeftChild() + "(" + getHeight(node.getLeftChild()) + "," + getBalance(node.getLeftChild()) + "), " : "null,");
+            System.out.print(node.getRightChild() != null ? node.getRightChild() + "(" + getHeight(node.getRightChild()) + "," + getBalance(node.getRightChild()) + ")\n" : "null\n");
+        }
+
+        private void inOrder(Node<T> node) {
+            if(node == null)
+                return;
+            inOrder(node.getLeftChild());
+            System.out.println(node);
+            inOrder(node.getRightChild());
+        }
+
+        /**
+         * wrapper to start recursive function
+         */
+        public void inOrder() {
+            inOrder(this.rootNode);
+        }
+
+        private int getHeight(Node<T> node) {
+            if(node == null) {
+                return 0;
+            }
+            int leftTHeight = getHeight(node.getLeftChild());
+            int rightTHeight = getHeight(node.getRightChild());
+            return Math.max(leftTHeight, rightTHeight) + 1;
+        }
+
+        private int getBalance(Node<T> node) {
+            return getHeight(node.getLeftChild()) - getHeight(node.getRightChild());
+        }
+
+        public void insert(T value) {
+            Node<T> newNode = new Node<>(value);
+            Node<T> node = this.rootNode;
+            
+            // if tree is empty (start with rootNode)
+            if(this.isEmpty()) {
+                this.rootNode = newNode;
+                return;
+            }
+            // search for place to insert and insert it (not if key already existing)
+            while(true) {
+                if(node.compareTo(newNode) <= -1) {
+                    if(node.getRightChild() == null) {
+                        node.setRightChild(newNode);
+                        return;
+                    }
+                    else {
+                        node = node.getRightChild();
+                    }
+                }
+                else if(node.compareTo(newNode) >= 1) {
+                    if(node.getLeftChild() == null) {
+                        node.setLeftChild(newNode);
+                        return;
+                    }
+                    else {
+                        node = node.getLeftChild();
+                    }
+                    
+                }
+                else {
+                    System.out.println("Search Tree contains already node with this key!");
+                    return;
+                }
+            } 
+        }
+
+        public void modify(T searchKey, T newKey) {
+            Node<T> modifyNode = new Node<>(searchKey);
+            Node<T> node = this.rootNode;
+
+            // search for element to modify
+            while(node != null && node.getValue() != modifyNode.getValue()) {
+                if(node.compareTo(modifyNode) >= 1) {
+                    node = node.getLeftChild();
+                }
+                else {
+                    node = node.getRightChild();
+                }
+            }
+            
+            // element to modify was not found
+            if(node == null) {
+                return;
+            }
+            
+            // modify element
+            delete(node.getValue());
+            modifyNode.setValue(newKey);
+            insert(modifyNode.getValue());
+        }
+
+        public void delete(T key) {
+            Node<T> deleteNode = new Node<>(key);
+            Node<T> node = this.rootNode;
+            Node<T> parent = null;
+
+            // search for element to delete (save parent element)
+            while(node != null && node.getValue() != deleteNode.getValue()) {
+                parent = node;
+                if(node.compareTo(deleteNode) >= 1) {
+                    node = node.getLeftChild();
+                }
+                else {
+                    node = node.getRightChild();
+                }
+            }
+
+            // element to delete was not found
+            if(node == null) {
+                return;
+            }
+            
+            // delete the selected element
+            if(node.getLeftChild() == null || node.getRightChild() == null) {
+                deleteNode(node, parent);
+            }
+            else {
+                deleteNodeTwoChilds(node);
+            }
+
+        }
+
+        /**
+         * max one child available - get available child and set it a level higher
+         */
+        private void deleteNode(Node<T> node, Node<T> parent) {
+            Node<T> child = node.getLeftChild() != null ? node.getLeftChild() : node.getRightChild();
+          
+            if (node == this.rootNode) {
+              this.rootNode = child;
+            } else if (node.compareTo(parent) >= 1) {
+                parent.setRightChild(child);
+            } else {
+                parent.setLeftChild(child);
+            }
+        }
+
+        /**
+         * find minimum node of right childtree
+         */
+        private void deleteNodeTwoChilds(Node<T> node) {
+            // Find minimum node of right childtree
+            Node<T> newNode = node.getRightChild();
+            Node<T> newNodeParent = node;
+            while (newNode.getLeftChild() != null) {
+                newNodeParent = newNode;
+                newNode = newNode.getLeftChild();
+            }
+          
+            // write new key to deleting element
+            node.setValue(newNode.getValue());
+          
+            // replacing element was right child
+            if (newNode.compareTo(node.getRightChild()) == 0) {
+                node.setRightChild(newNode.getRightChild());
+            }
+            else {
+                // if there is a subtree (only right) from minimum of right childtree
+                newNodeParent.setLeftChild(newNode.getRightChild());
+            }
+        }
+
         @Override
         public boolean equals(Object object) {
             if (object == null) return false;
